196d1d889c3415e1e6887aabdbeac645e8a7ad32
mohuishou <1@lailin.xyz>
2016-07-23 22:13:39 +0800

    【新增】需求文档

---
+++ Admin/db.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by mohuishou<1@lailin.xyz>.
+ * User: mohuishou<1@lailin.xyz>
+ * Date: 2016/7/16 0016
+ * Time: 20:52
+ */
+// 链接服务器
+$m = new MongoClient();
+
+// 选择一个数据库
+ 
